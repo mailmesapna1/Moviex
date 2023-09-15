7faecffab6df060669bf947a64211bcff8d41cc8
mailmesapna1 <mailmesapna1@gmail.com>
2023-09-15 23:56:26 +0530

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState,useEffect } from 'react'
 
 import {fetchDataFromApi} from"./utils/api";
+import {useSelector,useDispatch} from 'react-redux'
+import {getApiConfiguration} from "./store/homeSlice";
 
 function App() {
-
+  const dispatch = useDispatch();
   useEffect(()=>{
     apiTesting();
   },[])
@@ -12,6 +14,7 @@ function App() {
     fetchDataFromApi('/movie/popular')
     .then((res) =>{
       console.log(res);
+      dispatch(getApiConfiguration(res));
     })
   }
 
