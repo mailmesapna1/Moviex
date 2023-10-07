203f920be991567289f2d5ba3a78f14b77bfc1c0
mailmesapna1 <mailmesapna1@gmail.com>
2023-10-07 19:32:25 +0530

    3 Day

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {store} from "./store/store";
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider staore = {store}>
+  <Provider store = {store}>
     <App />
   </Provider>,
 )
